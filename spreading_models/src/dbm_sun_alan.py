d3f873aa44c41bb346a2493a4a1ddc1ac5f838ce
alansun17904 <asun17904@gmail.com>
2019-07-17 21:24:54 -0400

    formatted output to match problem requirments

--- spreading_models/src/dbm_sun_alan.py
+++ spreading_models/src/dbm_sun_alan.py
@@ -1,16 +1,20 @@
-import pprint
-from .deterministic import DeterministicGraph
+from deterministic import DeterministicGraph
+import os
 
 
 def read_graph(path):
     f_in = open(path)
-    num_of_nodes = int(f_in.readline().strip())
-    starting_node_num = int(f_in.readline().strip())
-    q_value = float(f_in.readline().strip())
+    num_of_nodes = int(f_in.readline().strip())  # total number of nodes
+    starting_node_num = int(f_in.readline().strip())  # starting number for node labels
+    q_value = float(f_in.readline().strip())  # q-value
+    # make initial infect a list and strip white space from each entry
     initial_infected = list(map(lambda s: s.strip(), f_in.readline().strip().split(',')))
+    # cast into integer and subtract by starting node number to create indices
     initial_infected = list(map(lambda s: int(s) - starting_node_num, initial_infected))
+    # list of all edges
     edges = list(map(lambda s: s.strip(), f_in.readlines()))
     edges = list(map(lambda s: s.split(','), edges))
+    # remove white space for each entry of
     edges = list(map(lambda s: list(map(lambda v: int(v.strip()) - starting_node_num, s)), edges))
     return [num_of_nodes, q_value, initial_infected, edges]
 
@@ -26,10 +30,14 @@ def make_adj_matrix(num_nodes, edges):
 
 if __name__ == '__main__':
     g = read_graph('../data/dbm_graded_input.txt')
-    adj = make_adj_matrix(g[0], g[3])
-    d = DeterministicGraph(g[1], g[2], adj)
+    adj_m = make_adj_matrix(g[0], g[3])
+    d = DeterministicGraph(g[1], g[2], adj_m)
     d.initialize_nodes()
-    for time in d.spread():
-        print(f't_{time[0]} = {time[1]}')
-        print(f'Number of Infected: {len(time[1])}')
+    time_table = d.spread()
+    previous_set = set()
+    for time in time_table:
+        print(f't{time[0]}: {list(time[1] - previous_set)}')
+        previous_set = time[1].copy()
+    print(f't{time_table[-1][0]}: []')
+    print(f'Final: {list(time_table[-1][1])}')
 
---
+++ spreading_models/src/edge.py
@@ -0,0 +1,3 @@
+class Edge:
+    def __init__(self, node1, node2, p):
+        pass
