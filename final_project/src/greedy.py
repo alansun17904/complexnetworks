c7cd54c80675024764a35a144cc40e5e32dfc101
alansun17904 <asun17904@gmail.com>
2019-07-25 16:11:29 -0400

    added new comments to submission files

--- final_project/src/greedy.py
+++ final_project/src/greedy.py
@@ -1,89 +1,112 @@
 import oop_icm
 import numpy as np
-import random
-import logging
 
 
 def greedy(node_list, linked_node_list):
     """
+    Greedy algorithm for finding the set of initially infected nodes to maximize spread
     :param node_list: list of nodes that are being considered for optimization
-    :return: a list of most optimal starting nodes
+    :return: a tuple of a list of most optimal starting nodes List[int] and a float with the average number
+    of nodes infected from this optimized list.
     """
     finding_max = True
     maximum_initial = []
     while finding_max:
-        local_max = []
-        avaliable_nodes = set(node_list) - set(maximum_initial)
-        for node in avaliable_nodes:
+        local_max = []  # List[Tuple(int, float)] contribution of each node + nodes from `maximum_initial`
+        available_nodes = set(node_list) - set(maximum_initial)  # nodes that are still available for selection
+        for node in available_nodes:
             # number of times icm is to be run
-            node_benefit = icm_wrapper(maximum_initial + [node], linked_node_list, .1, 5)
-            # logging.debug((maximum_initial + [node], node_benefit))
+            node_benefit = icm_wrapper(maximum_initial + [node], linked_node_list, .5, 10)
             local_max.append((node, node_benefit))
         local_max.sort(key=lambda n: n[1])
-        maximum_initial.append(local_max[-1][0])
+        maximum_initial.append(local_max[-1][0])  # get the node that added the largest value
         if len(maximum_initial) == 20:  # the maximum number of nodes that can be infected initially
-            return maximum_initial, icm_wrapper(maximum_initial, linked_node_list, 0.1, 1000)
+            return maximum_initial, icm_wrapper(maximum_initial, linked_node_list, 0.1, 100)
 
 
 def icm_wrapper(initial_nodes, linked_node_list, min_dev, min_iter):
+    """
+    Wrapper for ICM
+    :param initial_nodes: Initial nodes that are infected.
+    :param linked_node_list:
+    :param min_dev: The minimum standard deviation for the program to stop
+    :param min_iter: The minimum amount of iterations the program must run
+    :return: The average number of nodes are were infected
+    """
     count = 0
     std = 0
     run_log = []
+    # continue while the program does not satisfy the minimum number of runs or
+    # the standard deviation of the result from each run is above the std_dev threshold.
     while count < min_iter or std > min_dev:
         run_log.append(oop_icm.quick_run(linked_node_list, initial_nodes))
         if count >= min_iter:
             run_log.sort()
+            # standard deviation with the maximum and minimum value removed.
             std = np.std(run_log[1:len(run_log)-1])
         count += 1
+    # average with the first (min) and last (max) element of the sorted list removed.
     return sum(run_log[1:len(run_log) - 1]) / (len(run_log) - 2)
 
 
-def deeptop20outdegree(adj_matrix, num, beta):
-    outdegree = [(index, sum(v) + beta) for index, v in enumerate(adj_matrix)]
+def degree_katz_centrality(adj_matrix, num, beta):
+    """
+    :param adj_matrix: Adjacency matrix
+    :param num: The number of possible initial starting nodes returned.
+    :param beta: (float) dictates the beta value
+    :return: a list of indices for possible initial nodes.
+    """
+    # vector with the out degree of each node
+    out_degree = [(index, sum(v) + beta) for index, v in enumerate(adj_matrix)]
     adj_matrix = adj_matrix.copy()
     for row in range(len(adj_matrix)):
         for col in range(row):
             if adj_matrix[row][col] != 0:
-                adj_matrix[row][col] = outdegree[col][1] * (adj_matrix[row][col] + beta)
-    deepout = [(index, sum(v)) for index, v in enumerate(adj_matrix)]
-    deepout.sort(key=lambda n: n[1])
-    return deepout[-num:]
-
-
-def deeptop20outdegree_ordered(adj_matrix):
-    pass
-
-
-def shallowtop20outdegree(adj_matrix, num):
-    outdegree = [(index, sum(v)) for index, v in enumerate(adj_matrix)]
-    outdegree.sort(key=lambda n: n[1])
-    return outdegree[-num:]
-
-
-def matrix_power(adj_matrix, num, pow):
-    matrixpow = []
-    top = np.zeros(len(adj_matrix))
-    for i in range(50):
-        matrixpow.append(np.linalg.matrix_power(adj_matrix, i))
-    for i in range(50):
-        toprow = []
-        for index, matrix in enumerate(matrixpow):
-            toprow.append(sum(matrixpow[i][index]))
+                # centrality is the summation of the importance of adjacent nodes
+                # multiplied by the probability of infection of that nodes + a beta value
+                adj_matrix[row][col] = out_degree[col][1] * (adj_matrix[row][col] + beta)
+    # creates a katz centrality vector from the newly formed adjacency matrix
+    katz = [(index, sum(v)) for index, v in enumerate(adj_matrix)]
+    katz.sort(key=lambda n: n[1])
+    return katz[-num:]
 
 
 
-if __name__ == '__main__':
-    logging.basicConfig(filename='../logging/results.log', level=logging.DEBUG,
-                        format='<%(asctime)s>: %(message)s')
+def degree_centrality(adj_matrix, num):
+    """
+    Ranks nodes by the summation of their edge weights
+    :param adj_matrix: List[List] adjacency matrix
+    :param num: The number of nodes being returned
+    :return: List[int], nodes with top eigen weight edges.
+    """
+    out_degree = [(index, sum(v)) for index, v in enumerate(adj_matrix)]
+    out_degree.sort(key=lambda n: n[1])  # sort according to degree centrality
+    return out_degree[-num:]
 
-    # print(greedy(1000, '../data/new_graph1.txt'))
-    edges = oop_icm.process_edges(oop_icm.process_file('../data/cnngnbea.txt'))
+
+def eig(adj, num):
+    """
+    Calculates the eigen vector and returns a tuple with the top num eigen values.
+    [(v1, e1), (v2, e2), ... , (v_num, e_num)]
+    :param adj: Adjacency matrix
+    :param num: The number of greatest eigen values returned
+    :return: List[int] with length num of the greatest eigen values.
+    """
+    val, vec = np.linalg.eig(adj)
+    eig_central = vec[:, list(val).index(val.max())]  # find the eigen vector for the maximum eigen value
+    e = list(enumerate(eig_central))  # create tuple pairs of node index and eigen value
+    e.sort(key=lambda n: n[1])  # sort according to eigen value
+    return e[-num:]
+
+
+def pick_seeds(path):
+    """
+    :param path: Path to input file
+    :return: List[int] of optimized initial nodes.
+    """
+    edges = oop_icm.process_edges(oop_icm.process_file(path))
     linked_node_list = oop_icm.generate_node_list(edges, 1000)
     adj = oop_icm.generate_adjacency_matrix(edges, 1000)
-    t_20 = deeptop20outdegree(adj, 35, 0.4)
-
-    logging.info(greedy([v[0] for v in t_20], linked_node_list))
-
-    # print(icm_wrapper([v[0] for v in t_20], linked_node_list, 0.1, 1000))
-
+    top = eig(adj, 35)
+    return greedy([v[0] for v in top], linked_node_list)[0]
 
--- final_project/src/oop_icm.py
+++ final_project/src/oop_icm.py
@@ -2,91 +2,111 @@ import numpy as np
 import random
 
 
-# generate adjacency matrix
 def generate_adjacency_matrix(edge_list, n):
+    """
+    generate adjacency matrix
+    :param edge_list:
+    :param n:
+    :return:
+    """
     adjacency_matrix = np.zeros((n, n))
     for edge in edge_list:
         adjacency_matrix[edge[0]][edge[1]] = edge[2]
     return adjacency_matrix
 
 
-# returns a generated Node object list in which each node has its own children
 def generate_node_list(edge_list, n):
+    """
+    :param edge_list: Edge list created from `process_edges` (List[List]) in the format of
+    [[v1, v2, p1], [v1, v3, p2], ... [v_n, v_m, p_n]]
+    where v_n marks the starting node, v_m marks the end node, and p_n denotes the
+    probability of infection spreading from v_n to v_m
+    :param n: The number of nodes in the graph
+    :return: A list of node objects
+    """
     adj_mat = generate_adjacency_matrix(edge_list, n)
     # instantiate nodes
     node_list = [Node(i) for i in range(n)]
     # make connections
     for edge in edge_list:
-        node_list[edge[0]].add_child(node_list[edge[1]],
-                                     adj_mat[edge[0]][edge[1]])
+        node_list[edge[0]].add_child(node_list[edge[1]], adj_mat[edge[0]][edge[1]])
     return node_list
 
 
-# processes a list of edges and convert them from string to integer
-# split the pairs of edges by comma and strips any leading / trailing spaces
 def process_edges(edges_string_list):
-    return_list = []
+    """
+    processes a list of edges and convert them from string to integer
+    split the pairs of edges by comma and strips any leading / trailing spaces
+    :param edges_string_list:
+    :return: A list of all the edges List[List]
+    [[v1, v2, p1], [v1, v3, p2], ... [v_n, v_m, p_n]]
+    """
+    edge_list = []
     for line in edges_string_list:
         pair = line.split(',')
-        return_list.append([int(pair[0]), int(pair[1]), float(pair[2])])
-    return return_list
+        edge_list.append([int(pair[0]), int(pair[1]), float(pair[2])])
+    return edge_list
 
 
-# read, interpret and store contents from a file
 def process_file(file_name):
-    return_list = []
+    """
+    Strips whitespace from each line.
+    :param file_name: path to file
+    :return: A list of all the lines of the file.
+    """
     f_in = open(file_name)
     return list(map(lambda s: s.strip(), f_in.readlines()))
-    # with open(file_name) as file:  # open file and store contents in inFile
-    #     for line in file:
-    #         if line.count('\n') > 0:
-    #             # strip '\n' off
-    #             return_list.append(line[:-1])
-    #         else:
-    #             return_list.append(line)
-    # return return_list
 
 
 def quick_run(node_list, seeds):
+    """
+    Finds the number of infected nodes using the ICM model.
+    :param node_list: List of all node objects in the graph
+    :param seeds: List[int] initially infected nodes
+    :return: an int denoting the number of nodes infected
+    """
     all_nodes = node_list.copy()
     for node in all_nodes:
         node.heal()
     for seed_num in seeds:
         all_nodes[seed_num].infect()
-    count_sum = 0
     return sum([1 if node.infected else 0 for node in all_nodes])
-    # for node in all_nodes:
-    #     if node.infected:
-    #         count_sum += 1
-    # return count_sum
 
 
 class Node(object):
-
-    def __init__(self, n):
+    """
+    Represents a node
+    """
+    def __init__(self, n: int):
+        """
+        :param n: The index of the node [int]
+        """
         self.n = n
         self.child = []
         self.infected = False
 
-    def add_child(self, cd, wt):
+    def add_child(self, cd, wt: float):
+        """
+        Adds the child node and the edge weight connecting the two nodes to a list of child nodes.
+        :param cd: Child node (Node object)
+        :param wt: (float) the weight of the edge connecting the two nodes
+        :return: None
+        """
         self.child.append([cd, wt])
 
-    def get_index(self):
-        return self.n
-
     def infect(self):
+        """
+
+        :return:
+        """
         self.infected = True
         for cd in self.child:
             if not cd[0].infected and random.random() < cd[1]:
                 cd[0].infect()
 
     def heal(self):
+        """
+        Resets the state of the node to 'not infected'
+        :return:
+        """
         self.infected = False
-
-
-# # process read-in file and store in an array
-# inFile = process_file('../data/new_graph1.txt')
-# directed_edge_list = process_edges(inFile)
-# linked_node_list = generate_node_list(directed_edge_list)
-# trials = [quick_run(linked_node_list, [1]) for v in range(100)]
-# print(sum(trials) / len(trials))
